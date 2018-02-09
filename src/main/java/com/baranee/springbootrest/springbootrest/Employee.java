ff8ac077bff4d3094931271ae7efc9507855a549
ManivasakamR <manivasakamr@gmail.com>
2018-02-09 16:32:02 +0530

    fc1

---
+++ src/main/java/com/baranee/springbootrest/springbootrest/Employee.java
@@ -0,0 +1,104 @@
+package com.baranee.springbootrest.springbootrest;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; 
+	
+	@Column
+	private String name; 
+	
+	@Column
+	private String degree; 
+	
+	@Column
+	private Date dob; 
+	
+	@Column
+	private String father;
+
+	@Column
+	private Integer phone;
+
+	@Column
+	private String email;
+
+	@Column
+	private String address;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDegree() {
+		return degree;
+	}
+
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getFather() {
+		return father;
+	}
+
+	public void setFather(String father) {
+		this.father = father;
+	} 
+	
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	} 
+	
+	
+	
+	
+	
+}
